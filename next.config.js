ed69d3eb53a243e86d50df817bd949265ae78501
Marco <marco@mojica.de>
2020-09-12 17:59:26 +0200

    Update project config files

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    webpackDevMiddleware: (config) => {
+      // Solve compiling problem via vagrant
+      config.watchOptions = {
+        poll: 1000,   // Check for changes every second
+        aggregateTimeout: 300,   // delay before rebuilding
+      };
+      return config;
+    }
+  };
