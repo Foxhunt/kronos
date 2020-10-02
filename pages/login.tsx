c60db1c2f041237edcce3a9fe8b43f50e3b83271
Marco <marco@mojica.de>
2020-10-02 23:31:05 +0200

    Set userID before longing login Event

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,29 @@
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import { useRouter } from 'next/router'
+
 import firebase from "../firebase/clientApp"
 
-const uiConfig: firebaseui.auth.Config = {
-    signInSuccessUrl: "/",
-    credentialHelper: 'none',
-    signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    tosUrl: "/",
-    privacyPolicyUrl: "/"
-}
-
 export default function Login() {
-    return (typeof window !== 'undefined') ? <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/> : null
+    const router = useRouter()
+    const uiConfig: firebaseui.auth.Config = {
+        signInSuccessUrl: "/",
+        credentialHelper: 'none',
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            signInSuccessWithAuthResult: authResult => {
+                firebase.analytics().setUserId(authResult.user.uid)
+                if (authResult.additionalUserInfo.isNewUser) {
+                    firebase.analytics().logEvent(firebase.analytics.EventName.SIGN_UP, { method: firebase.auth.EmailAuthProvider.PROVIDER_ID })
+                }
+                router.push("/")
+                return false
+            }
+        },
+        tosUrl: "/",
+        privacyPolicyUrl: "/"
+    }
+
+    return (typeof window !== 'undefined') ? <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} /> : null
 }
