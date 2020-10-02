7fc1dadd4ab584b2a0d34be02756fe40dae424ea
Marco <marco@mojica.de>
2020-10-02 22:35:53 +0200

    Enable firestore persistence

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -18,6 +18,8 @@ const clientCredentials = {
 // Check that `window` is in scope for the analytics module!
 if (typeof window !== 'undefined' && !firebase.apps.length) {
     firebase.initializeApp(clientCredentials)
+    firebase.firestore().enablePersistence()
+
     // To enable analytics. https://firebase.google.com/docs/analytics/get-started
     if ('measurementId' in clientCredentials) firebase.analytics()
 }
