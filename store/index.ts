eab22fb70913e87a95877476f6defda523f685c0
Marco <marco@mojica.de>
2020-10-24 14:19:37 +0200

    Enable path setting

--- store/index.ts
+++ store/index.ts
@@ -5,15 +5,22 @@ export const userDocRefAtom = atom<firebase.firestore.DocumentReference | undefi
 export const selectedClientDocRefAtom = atom<firebase.firestore.DocumentSnapshot | undefined>(undefined)
 export const selectedProjectDocRefAtom = atom<firebase.firestore.DocumentSnapshot | undefined>(undefined)
 export const selectedTaskDocRefAtom = atom<firebase.firestore.DocumentSnapshot | undefined>(undefined)
-
-export const pathAtom = atom(get => {
-    const clientName = get(selectedClientDocRefAtom)?.get("name")
-    const projectName = get(selectedProjectDocRefAtom)?.get("name")
-    const taskName = get(selectedTaskDocRefAtom)?.get("name")
-
-    const path = [clientName, projectName, taskName].filter(crumb => crumb !== undefined)
-
-    return path.length ? path.join(" / ") : "Folders"
-})
-
 export const selectedCollectionDocRefAtom = atom<firebase.firestore.DocumentSnapshot | undefined>(undefined)
+
+export const pathAtom = atom(
+    get => {
+        const clientName = get(selectedClientDocRefAtom)?.get("name")
+        const projectName = get(selectedProjectDocRefAtom)?.get("name")
+        const taskName = get(selectedTaskDocRefAtom)?.get("name")
+
+        const path = [clientName, projectName, taskName].filter(crumb => crumb !== undefined)
+
+        return path.length ? path.join(" / ") : "Folders"
+    },
+    (_, set, [client, project, task, collection]) => {
+        set(selectedClientDocRefAtom, client)
+        set(selectedProjectDocRefAtom, project)
+        set(selectedTaskDocRefAtom, task)
+        set(selectedCollectionDocRefAtom, collection)
+    }
+)
