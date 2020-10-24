f70944a2e4442736612e31fe5f3203624a0fcbc2
Marco <marco@mojica.de>
2020-10-24 14:18:55 +0200

    Move Navigation into Header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,43 @@
-import Navigation from "./Navigation"
-import { userDocRefAtom } from "../../store"
+import firebase from "../../firebase/clientApp"
+import Link from "next/link"
+import styled from "styled-components"
 import { useAtom } from "jotai"
+import { userDocRefAtom } from "../../store"
+
+const Navigation = styled.nav`
+    height: 25px;
+    flex-shrink: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
+    background-color: black;
+
+    & a {
+        text-decoration: none;
+        color: white;
+    }
+`
 
 export default function Header() {
     const [userDocRef] = useAtom(userDocRefAtom)
 
     return <header>
-        <Navigation
-            loggedIn={userDocRef !== undefined} />
+        <Navigation>
+            <Link href={"/"}><a>home</a></Link >
+            <Link href={"/files"}><a>files</a></Link >
+            <Link href={"/favorites"}><a>favorites</a></Link >
+            <Link href={"/info"}><a>info</a></Link >
+            {userDocRef ?
+                <Link href={"/login"}>
+                    <a onClick={() => { firebase.auth().signOut() }}> logout</a>
+                </Link >
+                :
+                <Link href={"/login"}>
+                    <a>login</a>
+                </Link>
+            }
+        </Navigation>
     </header>
 }
