0dd855104957a60723d8d9bb55d1a096cc56bdd8
Marco <marco@mojica.de>
2020-11-08 23:55:15 +0100

    Add SVG Icons

--- next.config.js
+++ next.config.js
@@ -3,13 +3,21 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-    webpackDevMiddleware: (config) => {
+    webpackDevMiddleware(config) {
       // Solve compiling problem via vagrant
       config.watchOptions = {
         poll: 1000,   // Check for changes every second
         aggregateTimeout: 300,   // delay before rebuilding
       };
       return config;
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      });
+  
+      return config
     }
   })
   
