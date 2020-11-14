6108a7c94de830f1b786d85a06f6edfaf7044cbe
Marco <marco@mojica.de>
2020-11-15 00:20:53 +0100

    Refactor login
    create user document with cloud function

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -1,32 +1,63 @@
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 import firebase from "../firebase/clientApp"
 
 export default function login() {
     const router = useRouter()
-    const uiConfig: firebaseui.auth.Config = {
-        credentialHelper: 'none',
-        signInOptions: [
-            firebase.auth.EmailAuthProvider.PROVIDER_ID
-        ],
-        callbacks: {
-            signInSuccessWithAuthResult: authResult => {
-                firebase.analytics().setUserId(authResult.user.uid)
-                if (authResult.additionalUserInfo.isNewUser) {
-                    firebase.firestore().collection("users").doc(authResult.user.uid).set({
-                        name: authResult.user.displayName,
-                        email: authResult.user.email
-                    })
-                    firebase.analytics().logEvent(firebase.analytics.EventName.SIGN_UP, { method: firebase.auth.EmailAuthProvider.PROVIDER_ID })
-                }
-                router.push("/")
-                return false
-            }
-        },
-        tosUrl: "/",
-        privacyPolicyUrl: "/"
-    }
 
-    return (typeof window !== 'undefined') ? <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} /> : null
+    const [login, setLogin] = useState(true)
+    const [eMail, setEMail] = useState("")
+    const [password, setPassword] = useState("")
+
+    return <form
+        onSubmit={async event => {
+            event.preventDefault()
+            let credentials: firebase.auth.UserCredential | undefined = undefined
+            try {
+                if (login) {
+                    credentials = await firebase.auth().signInWithEmailAndPassword(eMail, password)
+                } else {
+                    credentials = await firebase.auth().createUserWithEmailAndPassword(eMail, password)
+                }
+            } catch (error) {
+                if (error.code === "auth/user-not-found") {
+                    setLogin(false)
+                }
+            } finally {
+                if (credentials?.user) {
+                    firebase.analytics().setUserId(credentials.user.uid)
+                    if (credentials?.additionalUserInfo?.isNewUser) {
+                        firebase.analytics().logEvent(
+                            firebase.analytics.EventName.SIGN_UP,
+                            { method: firebase.auth.EmailAuthProvider.PROVIDER_ID }
+                        )
+                    }
+                    router.push("/")
+                }
+            }
+        }}>
+        <div>
+            <label>E-Mail</label>
+            <input
+                type="email"
+                value={eMail}
+                onChange={event => {
+                    setEMail(event.target.value)
+                }} />
+        </div>
+        <div>
+            <label>Password</label>
+            <input
+                type="password"
+                value={password}
+                onChange={event => {
+                    setPassword(event.target.value)
+                }} />
+        </div>
+        {!login && <>User not found, want to sign up?<br /></>}
+        <button type="submit">
+            {login ? "Login" : "Sign Up"}
+        </button>
+    </form>
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions"
+import * as admin from "firebase-admin"
+admin.initializeApp()
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
+export const createNewUser = functions.auth.user().onCreate(user => {
+    admin.firestore().collection("users").doc(user.uid).set({
+        email: user.email
+    })
+    functions.logger.info(`createrd user ${user.email}`)
+})
