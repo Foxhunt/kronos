11c20b8914fccc0c895ac2f5ed0282f761f91f4e
Marco <marco@mojica.de>
2020-11-15 19:17:03 +0100

    Refactor login page

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -1,7 +1,13 @@
+import styled from "styled-components"
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 
 import firebase from "../firebase/clientApp"
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+`
 
 export default function login() {
     const router = useRouter()
@@ -10,54 +16,56 @@ export default function login() {
     const [eMail, setEMail] = useState("")
     const [password, setPassword] = useState("")
 
-    return <form
-        onSubmit={async event => {
-            event.preventDefault()
-            let credentials: firebase.auth.UserCredential | undefined = undefined
-            try {
-                if (login) {
-                    credentials = await firebase.auth().signInWithEmailAndPassword(eMail, password)
-                } else {
-                    credentials = await firebase.auth().createUserWithEmailAndPassword(eMail, password)
-                }
-            } catch (error) {
-                if (error.code === "auth/user-not-found") {
-                    setLogin(false)
-                }
-            } finally {
-                if (credentials?.user) {
-                    firebase.analytics().setUserId(credentials.user.uid)
-                    if (credentials?.additionalUserInfo?.isNewUser) {
-                        firebase.analytics().logEvent(
-                            firebase.analytics.EventName.SIGN_UP,
-                            { method: firebase.auth.EmailAuthProvider.PROVIDER_ID }
-                        )
+    return <Container>
+        <form
+            onSubmit={async event => {
+                event.preventDefault()
+                let credentials: firebase.auth.UserCredential | undefined = undefined
+                try {
+                    if (login) {
+                        credentials = await firebase.auth().signInWithEmailAndPassword(eMail, password)
+                    } else {
+                        credentials = await firebase.auth().createUserWithEmailAndPassword(eMail, password)
                     }
-                    router.push("/")
+                } catch (error) {
+                    if (error.code === "auth/user-not-found") {
+                        setLogin(false)
+                    }
+                } finally {
+                    if (credentials?.user) {
+                        router.push("/")
+                        firebase.analytics().setUserId(credentials.user.uid)
+                        if (credentials?.additionalUserInfo?.isNewUser) {
+                            firebase.analytics().logEvent(
+                                firebase.analytics.EventName.SIGN_UP,
+                                { method: firebase.auth.EmailAuthProvider.PROVIDER_ID }
+                            )
+                        }
+                    }
                 }
-            }
-        }}>
-        <div>
-            <label>E-Mail</label>
-            <input
-                type="email"
-                value={eMail}
-                onChange={event => {
-                    setEMail(event.target.value)
-                }} />
-        </div>
-        <div>
-            <label>Password</label>
-            <input
-                type="password"
-                value={password}
-                onChange={event => {
-                    setPassword(event.target.value)
-                }} />
-        </div>
-        {!login && <>User not found, want to sign up?<br /></>}
-        <button type="submit">
-            {login ? "Login" : "Sign Up"}
-        </button>
-    </form>
+            }}>
+            <div>
+                <label>E-Mail</label>
+                <input
+                    type="email"
+                    value={eMail}
+                    onChange={event => {
+                        setEMail(event.target.value)
+                    }} />
+            </div>
+            <div>
+                <label>Password</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={event => {
+                        setPassword(event.target.value)
+                    }} />
+            </div>
+            {!login && <>User not found, want to sign up?<br /></>}
+            <button type="submit">
+                {login ? "Login" : "Sign Up"}
+            </button>
+        </form>
+    </Container>
 }
