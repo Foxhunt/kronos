19c2684583825c01181799818e5a2461124c3b98
Marco <marco@mojica.de>
2020-11-15 19:22:03 +0100

    Fix compile error

--- app/components/TasksAndFiles/FileGrid/TagList.tsx
+++ app/components/TasksAndFiles/FileGrid/TagList.tsx
@@ -1,5 +1,5 @@
 import firebase from "../../../firebase/clientApp"
-import { MouseEvent, useState } from "react"
+import { useState } from "react"
 import styled from "styled-components"
 import { motion, Variants } from "framer-motion"
 
