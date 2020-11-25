7320989ff21be88e0f1ca8177b435673c9362f7a
Marco <marco@mojica.de>
2020-11-25 23:17:57 +0100

    Add and use "@google-cloud/vision
    https://firebase.google.com/docs/functions/callable?authuser=0#call_the_function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,8 @@ import * as functions from "firebase-functions"
 import * as admin from "firebase-admin"
 admin.initializeApp()
 
+import { ImageAnnotatorClient } from "@google-cloud/vision"
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
@@ -11,6 +13,28 @@ export const createNewUser = functions.region("europe-west1").auth.user().onCrea
             email: user.email
         })
         functions.logger.info(`createrd user ${user.email}`)
+    } catch (error) {
+        functions.logger.error(error)
+    }
+})
+
+export const labelImage = functions.region("europe-west1").https.onCall(async data => {
+    try {
+        const snapshot = await admin.firestore().doc(data.path).get()
+        const bucket = admin.storage().bucket()
+        const filePath = snapshot.get("fullPath")
+        const fileRef = bucket.file(filePath)
+        const [metadata] = await fileRef.getMetadata()
+
+        //@ts-ignore
+        if (metadata?.contentType?.startsWith("image/")) {
+            const client = new ImageAnnotatorClient()
+            const [result] = await client.labelDetection(`gs://${bucket.name}/${filePath}`)
+
+            await snapshot.ref.update({
+                mlLabels: result.labelAnnotations
+            })
+        }
     } catch (error) {
         functions.logger.error(error)
     }
