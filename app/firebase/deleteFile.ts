1e7ad0cae5bd54ff85ab67b3a6b86d198067c55f
Marco <marco@mojica.de>
2020-12-17 14:57:19 +0100

    Refactor firebase functions

--- app/firebase/deleteFile.ts
+++ app/firebase/deleteFile.ts
@@ -1,14 +1,11 @@
 import firebase from "./clientApp"
 
 export default async function deleteFile(fileDocSnap: firebase.firestore.DocumentSnapshot) {
-    fileDocSnap.ref.delete().then(() => {
-        const storage = firebase.storage()
-        const fileRef = storage.ref(fileDocSnap.get("fullPath"))
-        fileRef.delete()
+    await fileDocSnap.ref.delete()
+    await firebase.storage().ref(fileDocSnap.get("fullPath")).delete()
 
-        firebase.analytics().logEvent("delete_file", {
-            name: fileDocSnap.get("name"),
-            fullPath: fileDocSnap.get("fullPath")
-        })
+    firebase.analytics().logEvent("delete_file", {
+        name: fileDocSnap.get("name"),
+        fullPath: fileDocSnap.get("fullPath")
     })
 }
--- app/firebase/uploadFile.ts
+++ app/firebase/uploadFile.ts
@@ -25,6 +25,7 @@ export default async function uploadFile(
                 resolve(await fileCollection.add({
                     name: fileRef.name,
                     favorite: false,
+                    marked: false,
                     client: client.ref,
                     project: project.ref,
                     task: task.ref,
