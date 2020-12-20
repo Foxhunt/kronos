4893a41c1bd2b2b5c37de174ea99f61cd8e82abf
Marco <marco@mojica.de>
2020-12-20 16:22:07 +0100

    Change next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,25 +1,28 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 })
 
-module.exports = withBundleAnalyzer({
-    webpackDevMiddleware(config) {
-      // Solve compiling problem via vagrant
-      config.watchOptions = {
-        poll: 1000,   // Check for changes every second
-        aggregateTimeout: 300,   // delay before rebuilding
-      };
-      return config;
-    },
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-      });
-      return config
-    },
-    images: {
-      domains: ["firebasestorage.googleapis.com"]
-    }
-  })
+let config = {
+  webpackDevMiddleware(config) {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config
+  },
+  images: {
+    domains: ["firebasestorage.googleapis.com"]
+  }
+}
+config = withBundleAnalyzer(config)
+
+module.exports = config
   
