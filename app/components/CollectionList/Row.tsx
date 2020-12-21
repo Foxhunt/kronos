2d88ec3fafcf7395fa1219fe3a35d4903838b97c
Marco <marco@mojica.de>
2020-12-21 23:47:41 +0100

    Move Row into collection list

--- app/components/CollectionList/Row.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Row = styled.div`
-    display: grid;
-    grid-template-columns: repeat(7, 1fr);
-    grid-template-rows: 40px;
-
-    border-bottom: 1px solid black;
-`;
--- app/components/CollectionList/index.tsx
+++ app/components/CollectionList/index.tsx
@@ -1,9 +1,17 @@
+import styled from "styled-components"
 
 import { useTasks } from "../../hooks"
 import Collection from "./Collection"
-import { Row } from "./Row"
 import { Cell } from "./Cell"
 import { useState } from "react"
+
+export const Row = styled.div`
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: 40px;
+
+    border-bottom: 1px solid black;
+`
 
 export default function TaskList() {
     const [orderBy, setOrderBy] = useState("createdAt")
