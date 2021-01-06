d5b609dbbfd7c2407cf8911b6fd179de961e2fb3
Marco <marco@mojica.de>
2021-01-06 22:31:17 +0100

    Actualy commit useOfflineSearch changes

--- app/hooks/useOfflineSearch.ts
+++ app/hooks/useOfflineSearch.ts
@@ -1,27 +1,42 @@
+import firebase from "../firebase/clientApp"
 import Fuse from "fuse.js"
 import { useMemo } from "react"
 
-type props<T> = {
-    searchItems: T[]
+type props = {
+    searchDocuments: firebase.firestore.DocumentSnapshot<firebase.firestore.DocumentData>[]
     keys: string[]
     searchText: string
 }
 
-// make searchItems firestore.DocumentSnapshots and transform based on keys???
+export function useOfflineSearch({ searchDocuments, keys, searchText }: props) {
+    const searchItems = useMemo(() => searchDocuments.map(document => {
+        const item: any = {
+            id: document.id
+        }
 
-export function useOfflineSearch<T>({ searchItems: searchitems, keys, searchText }: props<T>) {
+        for (const key of keys) {
+            item[key] = document.get(key)
+        }
+
+        return item
+    }), [searchDocuments])
+
     const index = useMemo(() => {
-        const index = new Fuse(searchitems, {
+        const index = new Fuse(searchItems, {
             keys,
             threshold: 0.3
         })
 
         return index
-    }, [searchitems])
+    }, [searchItems])
 
     const searchResult = useMemo(() => {
         return index.search(searchText)
     }, [searchText, index])
 
-    return searchResult
+    const foundIDs = useMemo(() => {
+        return searchResult.map(result => result.item.id)
+    }, [searchResult])
+
+    return searchDocuments.filter(file => foundIDs.includes(file.id))
 }
