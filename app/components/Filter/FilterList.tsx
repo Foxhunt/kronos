690a9129fd9f003d63348c32d314542a0aa0c7be
Marco <marco@mojica.de>
2021-01-10 22:59:57 +0100

    Refactor Filter
    Align with Files

--- app/components/Filter/FilterList.tsx
+++ app/components/Filter/FilterList.tsx
@@ -1,3 +1,4 @@
+import { ComponentProps, ReactNode, useEffect, useRef, useState } from "react"
 import styled from "styled-components"
 
 export const Container = styled.div`
@@ -19,9 +20,65 @@ export const Item = styled.div<{ selected?: boolean }>`
         color: white;
         ` : ""
     }
+
+    &:last-child {
+        border-bottom: none;
+    }
 `
 
 export const Items = styled.div`
     max-height: calc(100% - 31px);
     overflow: auto;
+    
+    &::-webkit-scrollbar {
+        display: none;
+    }
 `
+
+const ScrollUpIndicator = styled.div`
+    position: sticky;
+
+    width: 100%;
+    text-align: center;
+    top: 0px;
+`
+const ScrollDownIndicator = styled.div`
+    position: sticky;
+
+    width: 100%;
+    text-align: center;
+    bottom: 0px;
+`
+
+interface props extends ComponentProps<"div"> {
+    name: string
+}
+
+export default function FilterList({ name, children }: props) {
+    const [canScrollUp, setCanScrollUp] = useState(false)
+    const [canScrollDown, setCanScrollDown] = useState(false)
+    const ItemsRef = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        if (ItemsRef.current) {
+            setCanScrollUp(ItemsRef.current.scrollTop >= 31)
+            setCanScrollDown(ItemsRef.current.scrollHeight - ItemsRef.current.scrollTop - ItemsRef.current.clientHeight >= 31)
+        }
+    }, [ItemsRef.current, children])
+
+    return <Container>
+        <Item>{name}</Item>
+        <Items
+            ref={ItemsRef}
+            onScroll={event => {
+                setCanScrollUp(event.currentTarget.scrollTop >= 31)
+                setCanScrollDown(event.currentTarget.scrollHeight - event.currentTarget.scrollTop - event.currentTarget.clientHeight >= 31)
+            }}>
+            {canScrollUp && <ScrollUpIndicator>up</ScrollUpIndicator>}
+            {children}
+            {new Array((children as Array<ReactNode>)?.length < 5 ? 5 - (children as Array<ReactNode>)?.length : 0)
+                .fill("")
+                .map((_item, index) => <Item key={index} />)}
+            {canScrollDown && <ScrollDownIndicator>down</ScrollDownIndicator>}
+        </Items>
+    </Container>
+}
--- app/components/Filter/index.tsx
+++ app/components/Filter/index.tsx
@@ -12,14 +12,14 @@ import {
     searchFileAtom
 } from "../../store"
 
-import { Container as ListContainer, Item, Items } from "./FilterList"
+import FilterList, { Item } from "./FilterList"
 
 const Container = styled.div`
     width: 100%;
 
     display: grid;
     grid-template-columns: repeat(4,1fr);
-    grid-template-rows: calc(6 * 31px);
+    grid-template-rows: calc(6 * 31px - 1px);
     
     border-bottom: 1px solid black;
 `
@@ -71,65 +71,59 @@ export default function Filter({ onHide }: props) {
             onChange={event => {
                 setSearchedFile(event.target.value)
             }} />
-        <ListContainer>
-            <Item>sort By</Item>
-            <Items>
-                {sortByOptions?.map(item =>
-                    <Item
-                        ref={orderBy === item ? selectedSortByItemRef : null}
-                        key={item.orderBy + item.orderDirection}
-                        selected={orderBy === item}
-                        onClick={() => {
-                            if (orderBy === item) {
-                                setOrderBy({ orderBy: "createdAt", orderDirection: "desc" })
-                            } else {
-                                setOrderBy(item)
-                            }
-                        }}>
-                        {item.orderBy}
-                    </Item>
-                )}
-            </Items>
-        </ListContainer>
-        <ListContainer>
-            <Item>Marked / Favorite</Item>
-            <Items>
+        <FilterList
+            name={"sort By"}>
+            {sortByOptions?.map(item =>
                 <Item
-                    selected={favorites}
+                    ref={orderBy === item ? selectedSortByItemRef : null}
+                    key={item.orderBy + item.orderDirection}
+                    selected={orderBy === item}
                     onClick={() => {
-                        setFavorties(!favorites)
-                    }}>
-                    favorites
-                </Item>
-                <Item
-                    selected={marked}
-                    onClick={() => {
-                        setMarked(!marked)
-                    }}>
-                    marked
-                </Item>
-            </Items>
-        </ListContainer>
-        <ListContainer>
-            <Item>Tags</Item>
-            <Items>
-                {tags.map(tag => <Item
-                    key={tag.id}
-                    selected={selectedTags.includes(tag.id)}
-                    onClick={() => {
-                        if (selectedTags.includes(tag.id)) {
-                            const newSelectedTags = [...selectedTags]
-                            newSelectedTags.splice(selectedTags.indexOf(tag.id), 1)
-                            setSelectedTags(newSelectedTags)
+                        if (orderBy === item) {
+                            setOrderBy({ orderBy: "createdAt", orderDirection: "desc" })
                         } else {
-                            const newSelectedTags = [...selectedTags]
-                            newSelectedTags.push(tag.id)
-                            setSelectedTags(newSelectedTags)
+                            setOrderBy(item)
                         }
                     }}>
-                    {tag.id}
-                </Item>)}
-            </Items>
-        </ListContainer>
-    </Container>
+                    {item.orderBy}
+                </Item>
+            )}
+        </FilterList >
+        <FilterList
+            name={"Marked / Favorite"}>
+            <Item
+                selected={favorites}
+                onClick={() => {
+                    setFavorties(!favorites)
+                }}>
+                favorites
+            </Item>
+            <Item
+                selected={marked}
+                onClick={() => {
+                    setMarked(!marked)
+                }}>
+                marked
+            </Item>
+        </FilterList>
+        <FilterList
+            name={"Marked / Favorite"}>
+            {tags.map(tag => <Item
+                key={tag.id}
+                selected={selectedTags.includes(tag.id)}
+                onClick={() => {
+                    if (selectedTags.includes(tag.id)) {
+                        const newSelectedTags = [...selectedTags]
+                        newSelectedTags.splice(selectedTags.indexOf(tag.id), 1)
+                        setSelectedTags(newSelectedTags)
+                    } else {
+                        const newSelectedTags = [...selectedTags]
+                        newSelectedTags.push(tag.id)
+                        setSelectedTags(newSelectedTags)
+                    }
+                }}>
+                {tag.id}
+            </Item>)}
+        </FilterList>
+    </Container >
 }
