d99c825d72c581bcf8cbec03c7b8c4f098473e6f
Marco <marco@mojica.de>
2021-01-29 17:19:01 +0100

    Style Filter

--- app/components/Filter/index.tsx
+++ app/components/Filter/index.tsx
@@ -18,12 +18,37 @@ import { Item } from "../Shared/ItemList"
 import Circle from "../Shared/Circle"
 
 const Container = styled.div`
+`
+
+const TextSearch = styled.input`
+    width: calc(100% - 5px);
+    height: 29px;
+    padding: unset;
+
+    padding-left: 5px;
+
+    border: none;
+    border-bottom: black 2px solid;
+    box-shadow: none;
+
+    font-family: "FuturaNowHeadline-Bd";
+
+    &::placeholder {
+        color: #dfdfe4;
+    }
+
+    &:focus {
+        outline: none!important;
+    }
+`
+
+const Selectibles = styled.div`
     width: 100%;
 
     display: grid;
-    grid-template-columns: repeat(4,1fr);
-    grid-template-rows: calc(6 * 31px - 1px);
-    
+    grid-template-columns: repeat(3,1fr);
+    grid-template-rows: calc(5 * 31px - 1px);
+
     border-bottom: 1px solid black;
 `
 
@@ -68,80 +93,89 @@ export default function Filter({ onHide }: props) {
 
     return <Container
         ref={containerRef}>
-        <input
+        <TextSearch
             type="text"
+            placeholder={"Type to Search"}
             value={searchedFile}
             onChange={event => {
                 setSearchedFile(event.target.value)
             }} />
-        <FilterList
-            name={"sort By"}>
-            {sortByOptions?.map(item =>
+        <Selectibles>
+            <FilterList
+                name={"sort By"}>
+                {sortByOptions?.map(item =>
+                    <Item
+                        ref={orderBy === item ? selectedSortByItemRef : null}
+                        key={item.orderBy + item.orderDirection}
+                        selected={false}
+                        onClick={() => {
+                            if (orderBy === item) {
+                                setOrderBy({ orderBy: "createdAt", orderDirection: "desc" })
+                            } else {
+                                setOrderBy(item)
+                            }
+                        }}>
+                        <Circle
+                            fill={orderBy === item ? "#000000" : "#ffffff"}
+                            stroke="#000000" />
+                        {item.orderBy}
+                    </Item>
+                )}
+            </FilterList >
+            <FilterList
+                name={"Marked / Favorite"}>
                 <Item
-                    ref={orderBy === item ? selectedSortByItemRef : null}
-                    key={item.orderBy + item.orderDirection}
-                    selected={orderBy === item}
+                    selected={false}
                     onClick={() => {
-                        if (orderBy === item) {
-                            setOrderBy({ orderBy: "createdAt", orderDirection: "desc" })
-                        } else {
-                            setOrderBy(item)
-                        }
+                        setFavorties(!favorites)
                     }}>
-                    {item.orderBy}
-                </Item>
-            )}
-        </FilterList >
-        <FilterList
-            name={"Marked / Favorite"}>
-            <Item
-                selected={favorites}
-                onClick={() => {
-                    setFavorties(!favorites)
-                }}>
-                <Circle
-                    fill={favorites ? "#0501ff" : "#00000000"}
-                    stroke="#0501ff" />
+                    <Circle
+                        fill={favorites ? "#0501ff" : "#00000000"}
+                        stroke="#0501ff" />
                 favorites
             </Item>
-            <Item
-                selected={marked}
-                onClick={() => {
-                    setMarked(!marked)
-                }}>
-                <Circle
-                    fill={marked ? "#33bd27" : "#00000000"}
-                    stroke="#33bd27" />
+                <Item
+                    selected={false}
+                    onClick={() => {
+                        setMarked(!marked)
+                    }}>
+                    <Circle
+                        fill={marked ? "#33bd27" : "#00000000"}
+                        stroke="#33bd27" />
                 marked
             </Item>
-        </FilterList>
-        <FilterList
-            name={"Tags"}>
-            {tags.map(tag => <Item
-                key={tag.id}
-                selected={selectedTags.includes(tag.id)}
-                onClick={() => {
-                    if (selectedTags.includes(tag.id)) {
-                        const newSelectedTags = [...selectedTags]
-                        newSelectedTags.splice(selectedTags.indexOf(tag.id), 1)
-                        setSelectedTags(newSelectedTags)
-                    } else {
-                        const newSelectedTags = [...selectedTags]
-                        newSelectedTags.push(tag.id)
-                        setSelectedTags(newSelectedTags)
-                    }
-                }}
-                onContextMenu={event => {
-                    event.preventDefault()
-                    tag.ref.delete()
-                    if (selectedTags.includes(tag.id)) {
-                        const newSelectedTags = [...selectedTags]
-                        newSelectedTags.splice(selectedTags.indexOf(tag.id), 1)
-                        setSelectedTags(newSelectedTags)
-                    }
-                }}>
-                {tag.id}
-            </Item>)}
-        </FilterList>
+            </FilterList>
+            <FilterList
+                name={"Tags"}>
+                {tags.map(tag => <Item
+                    key={tag.id}
+                    selected={false}
+                    onClick={() => {
+                        if (selectedTags.includes(tag.id)) {
+                            const newSelectedTags = [...selectedTags]
+                            newSelectedTags.splice(selectedTags.indexOf(tag.id), 1)
+                            setSelectedTags(newSelectedTags)
+                        } else {
+                            const newSelectedTags = [...selectedTags]
+                            newSelectedTags.push(tag.id)
+                            setSelectedTags(newSelectedTags)
+                        }
+                    }}
+                    onContextMenu={event => {
+                        event.preventDefault()
+                        tag.ref.delete()
+                        if (selectedTags.includes(tag.id)) {
+                            const newSelectedTags = [...selectedTags]
+                            newSelectedTags.splice(selectedTags.indexOf(tag.id), 1)
+                            setSelectedTags(newSelectedTags)
+                        }
+                    }}>
+                    <Circle
+                        fill={selectedTags.includes(tag.id) ? "#000000" : "#ffffff"}
+                        stroke="#000000" />
+                    {tag.id}
+                </Item>)}
+            </FilterList>
+        </Selectibles>
     </Container >
 }
