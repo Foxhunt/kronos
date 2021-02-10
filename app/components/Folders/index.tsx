b136ce5de93a50b15e15b7e39740a327bd80cebd
Marco <marco@mojica.de>
2021-02-10 21:28:54 +0100

    Add comment

--- app/components/Folders/index.tsx
+++ app/components/Folders/index.tsx
@@ -154,6 +154,7 @@ export default function Folders({ onHide }: props) {
                     setTask(newItem)
                     setBoard(undefined)
                 }} />}
+            {/* needs Cleanup! */}
             {boards && <FolderList
                 name={"boards"}
                 previousName={"level3"}
