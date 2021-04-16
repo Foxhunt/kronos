763a10de5df535f97bf54ed912ec2d22ff3d941f
Marco <marco@mojica.de>
2021-04-16 21:49:07 +0200

    Update login styles

--- app/pages/login.tsx
+++ app/pages/login.tsx
@@ -8,6 +8,20 @@ import firebase from "../firebase/clientApp"
 const Container = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
+`
+
+const Form = styled.form`
+    margin-top: 10px;
+`
+
+const Input = styled.input`
+    margin: 10px 0px;
+`
+
+const EnterMailHint = styled.div`
+    position: absolute;
+    margin: 10px 0px;
 `
 
 export default function login() {
@@ -37,7 +51,7 @@ export default function login() {
             <title>{accountExists ? "Login" : "Sign Up"}</title>
             <link rel="shortcut icon" href="/lock.svg" />
         </Head>
-        <form
+        <Form
             onSubmit={async event => {
                 event.preventDefault()
                 let credentials: firebase.auth.UserCredential | undefined = undefined
@@ -59,33 +73,37 @@ export default function login() {
                     }
                 }
             }}>
+            <label>
+                <div>
+                    Email
+                </div>
+                <Input
+                    type="email"
+                    value={eMail}
+                    required
+                    onChange={async event => {
+                        setEMail(event.target.value)
+                    }} />
+            </label>
+            <label>
+                <div>
+                    Password
+                </div>
+                <Input
+                    type="password"
+                    value={password}
+                    required
+                    minLength={6}
+                    onChange={event => {
+                        setPassword(event.target.value)
+                    }} />
+            </label>
             <div>
-                <label>
-                    Email <input
-                        type="email"
-                        value={eMail}
-                        required
-                        onChange={async event => {
-                            setEMail(event.target.value)
-                        }} />
-                </label>
+                <button type="submit">
+                    {accountExists ? "Login" : "Sign Up"}
+                </button>
             </div>
-            <div>
-                <label>
-                    Password <input
-                        type="password"
-                        value={password}
-                        required
-                        minLength={6}
-                        onChange={event => {
-                            setPassword(event.target.value)
-                        }} />
-                </label>
-            </div>
-            <button type="submit">
-                {accountExists ? "Login" : "Sign Up"}
-            </button>
-            {!accountExists && <><br />Enter your Email to login or sign up</>}
-        </form>
+            {!accountExists && <EnterMailHint>Enter your Email to login or sign up</EnterMailHint>}
+        </Form>
     </Container>
 }
