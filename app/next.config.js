8b9c227fa1f6a54566803663d0a4742ba0f3d9c1
Marco <marco@mojica.de>
2021-05-01 23:21:54 +0200

    Remove polling from webpack dev config

--- app/next.config.js
+++ app/next.config.js
@@ -1,28 +1,16 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
-})
+});
 
 let config = {
-  webpackDevMiddleware(config) {
-    // Solve compiling problem via vagrant
-    config.watchOptions = {
-      poll: 1000,   // Check for changes every second
-      aggregateTimeout: 300,   // delay before rebuilding
-    };
-    return config;
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ["@svgr/webpack"],
     });
-    return config
+    return config;
   },
-  images: {
-    domains: ["firebasestorage.googleapis.com"]
-  }
-}
-config = withBundleAnalyzer(config)
+};
+config = withBundleAnalyzer(config);
 
-module.exports = config
-  
+module.exports = config;
