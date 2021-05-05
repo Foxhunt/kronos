2fadb7061fe28611c408631ca95f24ecacab5c6a
Marco <marco@mojica.de>
2021-05-05 19:46:14 +0200

    Add firebase models

---
+++ app/@types/models.d.ts
@@ -0,0 +1,47 @@
+interface User {
+    email: string
+
+    //subcollections
+    collections: Collection[]
+}
+
+interface Collection {
+    name: string
+    createdAt: Date
+    lastUpdatedAt: Date
+    tags: string[]
+
+    //references
+    owner: User
+    sharedWith: User[]
+
+    //subcollections
+    boards: Board[]
+
+}
+interface Board {
+    name: string
+    createdAt: Date
+    lastUpdatedAt: Date
+    tags: string[]
+
+    //references
+    owner: User
+    collection: Collection
+
+    //subcollections
+    files: File[]
+}
+
+interface File {
+    name: string
+    createdAt: Date
+    lastUpdatedAt: Date
+    downloadURL: string
+    tags: string[]
+
+    //references
+    owner: User
+    collection: Collection
+    board: Board
+}
